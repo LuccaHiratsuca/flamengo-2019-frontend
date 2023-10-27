925e1e0a0aea4ccd569082a729186b58a6ed1214
Lucca Hiratsuca Costa <luccahiratsuca@gmail.com>
2023-10-27 17:08:25 -0300

    feat: constants Authentication
    Setting all the actions and status that we have when we're loggin in.

---
+++ src/store/constants/constantsAuthentication.ts
@@ -0,0 +1,14 @@
+export const AUTH_STATUS = {
+    STANDARD: '',
+    LOADING: 'AUTH_LOADING',
+    ERROR: 'AUTH_ERROR',
+    SUCCESS: 'AUTH_SUCCESS',
+  };
+  
+  export const AUTH_ACTIONS = {
+    STANDARD: '',
+    PENDING: 'LOGIN_PENDING',
+    FULFILLED: 'LOGIN_FULFILLED',
+    REJECTED: 'LOGIN_REJECTED',
+  };
+  
