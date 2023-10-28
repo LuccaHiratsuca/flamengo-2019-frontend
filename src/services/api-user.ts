ebe9975afd5d041300911961b8ad5c55d3e6a9b5
Lucca Hiratsuca Costa <luccahiratsuca@gmail.com>
2023-10-28 09:18:50 -0300

    feat: service api Zambom

--- src/services/api-user.ts
+++ src/services/api-user.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const authApi = createApi({
     reducerPath: 'authApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:6969/',
+        baseUrl: 'http://54.71.150.144:8082',
         prepareHeaders: (headers, { getState }) => {},
     }),
     endpoints: () => ({}),
