fb772538cd86052742635850066cb0b25268bb91
Lucca Hiratsuca Costa <luccahiratsuca@gmail.com>
2023-10-28 11:46:36 -0300

    feat: update Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import UserRoutes from './routes/User';
+import RoutesComponent from './routes/componentsRoutes';
 
 function App() {
     const queryClient = new QueryClient();
 
     return (
         <QueryClientProvider client={queryClient}>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/user/*" element={<UserRoutes />} />
-                    <Route path="*" element={<h1>404 - Not Found</h1>} />
-                </Routes>
-            </BrowserRouter>
+            <RoutesComponent />
         </QueryClientProvider>
     );
 }
