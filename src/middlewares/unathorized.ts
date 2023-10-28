c4e80b791b95e27bd683506e03560ac28012ffc5
Lucca Hiratsuca Costa <luccahiratsuca@gmail.com>
2023-10-28 11:08:51 -0300

    feat: middleware for unathorized token

---
+++ src/middlewares/unathorized.ts
@@ -0,0 +1,21 @@
+import { Middleware } from '@reduxjs/toolkit';
+import { RootState } from '../store';
+
+export const redirectMiddleware: Middleware = (store) => (next) => (action) => {
+    const STATE = (store.getState() as RootState);
+
+    const tokenState = STATE.token;
+    const authState = STATE.auth;
+    const currentPath = window.location.pathname;
+
+    if (authState.authenticationStatus === 'AUTH_ERROR'){
+        if (currentPath !== '/user/login'){
+            window.location.href = '/user/login';
+        }   
+    }
+
+    if ((tokenState.refreshTokenStatus === 'REFRESH_REJECTED')){
+        window.location.href = '/user/login';
+    }
+    return next(action);
+};
