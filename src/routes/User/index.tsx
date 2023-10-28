5ef4d15f61919e10a1735ab001c8867473123033
Lucca Hiratsuca Costa <luccahiratsuca@gmail.com>
2023-10-28 13:18:16 -0300

    fix: user routes

--- src/routes/User/index.tsx
+++ src/routes/User/index.tsx
@@ -1,12 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
 import Login from '../../pages/User/Login/Login';
-// import LoginScreen from '../../pages/Login';
 
 const UserRoutes = () => {
     return (
         <Routes>
             <Route path="/login" element={<Login />} />
-            <Route path="*" element={<h1>404 - User - Not Found</h1>} />
         </Routes>
     );
 }
