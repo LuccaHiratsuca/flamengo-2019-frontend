7237537f68c9760de2c021758baa02612ec0edfe
Lucca Hiratsuca Costa <luccahiratsuca@gmail.com>
2023-10-28 11:46:00 -0300

    feat: store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,33 +1,43 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit'
-import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
-import { persistStore, persistReducer } from 'redux-persist'
-import authReducer from './reducers/reducerAuthentication'
-import { authApi } from '../services/api-user'
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { persistStore, persistReducer } from 'redux-persist';
+import { authReducer } from './reducers/reducerAuthentication';
+import { authApi } from '../services/api-user';
+import { rentalApi } from '../services/api-rental';
+import { redirectMiddleware } from '../middlewares/unathorized';
+import { tokenReducer } from './reducers/reducerToken';
+import { rentalsReducer } from './reducers/reducerRentals';
 
 const persistConfig = {
   key: 'root',
   storage,
-  whitelist: ['auth'] // only auth will be persisted
-}
+  whitelist: ['auth', 'token'],
+};
 
 const rootReducer = combineReducers({
   auth: authReducer,
+  token: tokenReducer,
+  rentals: rentalsReducer,
   [authApi.reducerPath]: authApi.reducer,
-})
+  [rentalApi.reducerPath]: rentalApi.reducer,
+});
 
-const persistedReducer = persistReducer(persistConfig, rootReducer)
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) => 
+  reducer: persistedReducer, 
+  middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck:{
+      serializableCheck: {
         ignoredActions: ['persist/PERSIST'],
-      }
-    }).concat(authApi.middleware),
-})
+      },
+    })
+    .concat(authApi.middleware)
+    .concat(redirectMiddleware),
+});
 
-export const persistor = persistStore(store)
-export type AppDispatch = typeof store.dispatch
+export const persistor = persistStore(store);
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppDispatch = typeof store.dispatch;
 
-export default store
+export default store;
