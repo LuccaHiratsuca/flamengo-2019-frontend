356fc3a2a1ca85bed0aa0edc47a7cc4ceb2e5f19
Lucca Hiratsuca Costa <luccahiratsuca@gmail.com>
2023-10-28 11:10:18 -0300

    feat: component for check if the current token is valid

---
+++ src/components/AuthVerifier/AuthVerifier.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { verifyToken } from "../../store/actions/actionAuthentication";
+import { AppDispatch } from "../../store";
+import { useNavigate } from "react-router-dom";
+import { AuthState } from "../../types/interfaceAuthState";
+
+const AuthVerifier = () => {
+
+    const token: string | undefined = useSelector((state: { auth: AuthState }) => state.auth.userInfo?.token);
+    const dispatch: AppDispatch = useDispatch();
+    const navigate = useNavigate();
+    const [isLoading, setIsLoading] = useState(true);
+    
+    useEffect(() => {
+    
+      const handleVerifyToken = async () => {
+        if (token) {
+          try {
+            const action = await dispatch(verifyToken(token));
+            console.log(action.type);
+            if (action.type === verifyToken.fulfilled.type) {
+              setIsLoading(false);
+            } else {
+              navigate("/user/login");
+            }
+          } catch (error) {
+            console.error("Token verification failed", error);
+          }
+        }
+      };
+      
+      handleVerifyToken();
+    }, [token, dispatch, navigate]);
+  
+    if (isLoading) {
+      return <div>Loading...</div>;
+    }
+
+    return null;
+};
+
+export default AuthVerifier;
