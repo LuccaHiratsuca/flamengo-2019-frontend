68618fe5d13b0fbb3c135e4f3cdc8c81a68b7edd
Lucca Hiratsuca Costa <luccahiratsuca@gmail.com>
2023-10-28 13:50:20 -0300

    feat: api from our backend

--- src/services/api-rental.ts
+++ src/services/api-rental.ts
@@ -1,1 +1,11 @@
 // TO-DO: routes from out Backend
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const rentalApi = createApi({
+    reducerPath: 'rentalApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'http://localhost:8080',
+        prepareHeaders: (headers, { getState }) => {},
+    }),
+    endpoints: () => ({})
+})
