b794b0c161a572f3a7c306e553a979b70b5398d0
Lucca Hiratsuca Costa <luccahiratsuca@gmail.com>
2023-11-07 19:45:24 -0300

    fix: url backend rentals

--- src/services/api-rental.ts
+++ src/services/api-rental.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const rentalApi = createApi({
     reducerPath: 'rentalApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:8080',
+        baseUrl: 'http://18.236.255.187:8080',
         prepareHeaders: (headers, { getState }) => {},
     }),
     endpoints: () => ({})
