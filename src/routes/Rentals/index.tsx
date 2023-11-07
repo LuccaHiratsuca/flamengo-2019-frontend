6cd985e4154dd1aa0f4064db8245b3ed338abdd6
Lucca Hiratsuca Costa <luccahiratsuca@gmail.com>
2023-11-07 19:49:44 -0300

    fix: route of a new Rental

--- src/routes/Rentals/index.tsx
+++ src/routes/Rentals/index.tsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import {Rentals} from '../../pages/Rentals/Rentals/Rentals';
+import FormNewRental from '../../components/FormNewRental/FormNewRental';
 
 const RentalsRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<Rentals/>} />
             <Route path='/:id' element={<h1>Details</h1>} />
-            <Route path='/new' element={<h1>New</h1>} />
+            <Route path='/new' element={<FormNewRental/>} />
         </Routes>
     );        
 }
