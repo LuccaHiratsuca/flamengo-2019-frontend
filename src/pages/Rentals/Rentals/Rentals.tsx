7a1f96c5059c5599b2bdc2cf47989193c8d359e2
Lucca Hiratsuca Costa <luccahiratsuca@gmail.com>
2023-11-07 19:52:58 -0300

    fix: style Rentals page

--- src/pages/Rentals/Rentals/Rentals.tsx
+++ src/pages/Rentals/Rentals/Rentals.tsx
@@ -1,20 +1,19 @@
-import { Button } from "@mui/material"
-import  AuthVerifier from "../../../components/AuthVerifier/AuthVerifier"
-import ListRentals from "../../../components/ListRentals/ListRentals"
-import styles from "./Rentals.module.css"
+import { Button } from "@mui/material";
+import AuthVerifier from "../../../components/AuthVerifier/AuthVerifier";
+import ListRentals from "../../../components/ListRentals/ListRentals";
+import styles from "./Rentals.module.css";
+import { Header } from "../../../components/Header/Header";
 
 export const Rentals = () => {
-
     return (
       <>
         <AuthVerifier />
-        <h1> Rentals </h1>
-        <div>
-          <Button className={styles.greenButton} variant="contained" href="/rentals/new"> New Rental </Button>
-          <ListRentals /> 
+        <Header path="HomePage" />
+        <div className={styles.titleWithButton}>
+          <h1 className={styles.title}>Rentals</h1>
+          <Button className={styles.greenButton} variant="contained" href="/rentals/new">New Rental</Button>
         </div>
+        <ListRentals /> 
       </>
-
-    )
+    );
 }
-
